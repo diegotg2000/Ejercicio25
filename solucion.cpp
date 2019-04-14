b52eb0494a95b98a2c5e23166f2c628b76486e93
Diego Torres <diegotg2000@hotmail.com>
2019-04-14 21:32:41 +0000

    Se añadió la escritura de dos archivos

--- solucion.cpp
+++ solucion.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <fstream>
 #include <cmath>
+
+
 using namespace std;
 //La ecuación diferencial es: dy/dt = -wy, con y(0)=1 y es de t=0 hasta t=4/w
 
@@ -7,31 +10,46 @@ void explicit_Euler(float h);
 void implicit(float h);
 float w=4.0;
 
-int main{
+int main(){
     explicit_Euler(0.01/w);
-    implicit(0.01/w);    
+    implicit(0.01/w);   
+    return 0;
 }
 
 void explicit_Euler(float h){
+    ofstream outfile;
+
+    outfile.open("explicito.txt");
+
     float yn, yn1;
-    int N=4/(w*h)
+    int N=4/(w*h);
     yn=1;
-    cout<<0<<' '<<yn<<endl;
+    outfile<<0<<' '<<yn<<endl;
     for(int i=1;i<=N;i++){
         yn1=yn*(1-h*w);
         yn=yn1;
-        cout<<i*h<<' '<<yn<<endl;        
+        outfile<<i*h<<' '<<yn<<endl;        
     }
+    
+    outfile.close();
 }
 
 void implicit(float h){
+    ofstream outfile;
+
+    outfile.open("implicito.txt");
+
     float yn, yn1;
-    int N=4/(w*h)
+    int N=4/(w*h);
     yn=1;
-    cout<<0<<' '<<yn<<endl;
+    outfile<<0<<' '<<yn<<endl;
     for(int i=1;i<=N;i++){
-        yn1=yn/(1+h*w);
+        yn1=yn*(1-h*w);
         yn=yn1;
-        cout<<i*h<<' '<<yn<<endl;        
+        outfile<<i*h<<' '<<yn<<endl;        
     }
+    
+    outfile.close();
 }
+
+
